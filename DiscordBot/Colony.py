8f9237ae1eba74fdb86c940b4b242ac29589082b
ARO7I <ARO7I@icloud.com>
2021-02-14 15:58:39 +0900

    [DiscordBot] Colony 수정
    `discord.Embed`를 사용해 좀 더 깔끔하게 출력하도록 수정

--- DiscordBot/Colony.py
+++ DiscordBot/Colony.py
@@ -1,4 +1,5 @@
 from random import shuffle, choice
+from discord import Embed
 from discord.ext import commands
 from Token import Colony
 
@@ -14,17 +15,17 @@ async def on_ready():
 @bot.command(name="순서")
 async def 순서(ctx, *args):
     if(len(args) == 0):
-        await ctx.send("!순서 {1} {2} {3}...")
+        await ctx.send("```!순서 [args...]```")
     else:
-        arg = list(args)
-        shuffle(arg)
-        await ctx.send(" ".join(arg))
+        args = list(args)
+        shuffle(args)
+        await ctx.send(embed = Embed(title="순서", description=" - ".join(args)))
 
 @bot.command(name="뽑기")
 async def 뽑기(ctx, *args):
     if(len(args) == 0):
-        await ctx.send("!뽑기 {1} {2} {3}...")
+        await ctx.send("```!뽑기 [args...]```")
     else:
-        await ctx.send(choice(args))
+        await ctx.send(embed = Embed(title="뽑기", description=choice(args)))
 
 bot.run(Colony())
